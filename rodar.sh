5a2a7d61d08a519022ea8a948144e190ae0fe025
Wellson Almeida dos Santos <wellson.digital@gmail.com>
2026-01-19 13:33:13 -0300

    feat: enhance `rodar.sh` to automate virtual environment setup, dependency installation, and Jupyter kernel registration.

--- rodar.sh
+++ rodar.sh
@@ -1,7 +1,21 @@
 
+# cd /Users/wellsonalmeida/workspace/pessoal/fiap/pos/Tech-Challenge-Fase-1
+# pip install -r requirements.txt
+# jupyter notebook tech_challenge_fase1_dengue2022.ipynb
+
+# 1. Navegue até a pasta do projeto
 cd /Users/wellsonalmeida/workspace/pessoal/fiap/pos/Tech-Challenge-Fase-1
+# 2. Criar ambiente virtual
+python3 -m venv venv
+# 3. Ativar ambiente virtual
+source venv/bin/activate
+# 4. Instalar dependências
+pip install --upgrade pip
 pip install -r requirements.txt
-jupyter notebook tech_challenge_fase1_dengue2022.ipynb
+# 5. Registrar kernel no Jupyter
+python -m ipykernel install --user --name=tech_challenge_venv --display-name="Python (Tech Challenge)"
+# 6. Abrir o notebook
+jupyter notebook tech_challenge_fase1_dengue2022_w.ipynb
 
 
 
